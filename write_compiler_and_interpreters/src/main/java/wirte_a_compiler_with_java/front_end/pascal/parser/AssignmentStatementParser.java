0fcdcb52c00e74481661d9590df9ca7fd9764c65
hkzwhn <liuzhaocen@sina.com>
2023-02-05 21:06:50 -0500

    more parser

--- write_compiler_and_interpreters/src/main/java/wirte_a_compiler_with_java/front_end/pascal/parser/AssignmentStatementParser.java
+++ write_compiler_and_interpreters/src/main/java/wirte_a_compiler_with_java/front_end/pascal/parser/AssignmentStatementParser.java
@@ -4,8 +4,13 @@ import wirte_a_compiler_with_java.front_end.pascal.PascalParserTD;
 import wirte_a_compiler_with_java.front_end.token.Token;
 import wirte_a_compiler_with_java.intermediate.icode.ICodeFactory;
 import wirte_a_compiler_with_java.intermediate.icode.ICodeNode;
+import wirte_a_compiler_with_java.intermediate.symtabInterface.SymTabEntry;
 
+import static wirte_a_compiler_with_java.front_end.pascal.PascalErrorCode.MISSING_COLON_EQUALS;
+import static wirte_a_compiler_with_java.front_end.pascal.tokens.PascalTokenType.COLON_EQUALS;
+import static wirte_a_compiler_with_java.intermediate.icode.ICodeKeyImpl.ID;
 import static wirte_a_compiler_with_java.intermediate.icode.ICodeNodeTypeImpl.ASSIGN;
+import static wirte_a_compiler_with_java.intermediate.icode.ICodeNodeTypeImpl.VARIABLE;
 
 /**
  * @Author zhaocenliu
@@ -16,9 +21,28 @@ public class AssignmentStatementParser extends StatementParser {
         super(parent);
     }
 
-    public ICodeNode parse(Token token) throws Exception{
+    public ICodeNode parse(Token token) throws Exception {
         ICodeNode assignNode = ICodeFactory.createICodeNode(ASSIGN);
         String targetName = token.getText().toLowerCase();
-        return null;
+        SymTabEntry targetId = symTabStack.lookup(targetName);
+        if (targetId == null) {
+            targetId = symTabStack.enterLocal(targetName);
+        }
+        targetId.appendLineNumber(token.getLineNumber());
+        token = nextToken();
+        ICodeNode variableNode = ICodeFactory.createICodeNode(VARIABLE);
+        variableNode.setAttribute(ID, targetId);
+        assignNode.addChild(variableNode);
+        // Look for the := token.
+        if (token.getType() == COLON_EQUALS) {
+            token = nextToken(); // consume the :=
+        } else {
+            errorHandler.flag(token, MISSING_COLON_EQUALS, this);
+        }
+        // 解析表达式
+        ExpressionParser expressionParser = new ExpressionParser(this);
+        assignNode.addChild(expressionParser.parse(token));
+        return assignNode;
+
     }
 }
---
+++ write_compiler_and_interpreters/src/main/java/wirte_a_compiler_with_java/front_end/pascal/parser/ExpressionParser.java
@@ -0,0 +1,49 @@
+package wirte_a_compiler_with_java.front_end.pascal.parser;
+
+import wirte_a_compiler_with_java.front_end.pascal.PascalParserTD;
+import wirte_a_compiler_with_java.front_end.pascal.tokens.PascalTokenType;
+import wirte_a_compiler_with_java.front_end.token.Token;
+import wirte_a_compiler_with_java.intermediate.icode.ICodeNode;
+import wirte_a_compiler_with_java.intermediate.icode.ICodeNodeType;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+
+import static wirte_a_compiler_with_java.front_end.pascal.tokens.PascalTokenType.*;
+import static wirte_a_compiler_with_java.intermediate.icode.ICodeNodeTypeImpl.*;
+
+/**
+ * @Author zhaocenliu
+ * @create 2023/2/5 9:02 PM
+ */
+public class ExpressionParser extends StatementParser {
+    public ExpressionParser(PascalParserTD parent) {
+        super(parent);
+    }
+
+    public ICodeNode parse(Token token) throws Exception {
+        return parseExpression(token);
+    }
+
+    // Set of relational operators.
+    private static final EnumSet<PascalTokenType> REL_OPS =
+            EnumSet.of(EQUALS, NOT_EQUALS, LESS_THAN, LESS_EQUALS,
+                    GREATER_THAN, GREATER_EQUALS);
+    private static final HashMap<PascalTokenType, ICodeNodeType>
+            REL_OPS_MAP = new HashMap<PascalTokenType, ICodeNodeType>();
+
+    static {
+        REL_OPS_MAP.put(EQUALS, EQ);
+        REL_OPS_MAP.put(NOT_EQUALS, NE);
+        REL_OPS_MAP.put(LESS_THAN, LT);
+        REL_OPS_MAP.put(LESS_EQUALS, LE);
+        REL_OPS_MAP.put(GREATER_THAN, GT);
+        REL_OPS_MAP.put(GREATER_EQUALS, GE);
+    };
+
+    // TODO
+    private ICodeNode parseExpression(Token token){
+        return null;
+    }
+
+}
